bd90b1fe4a7ecbf3d463f79a94be19a3fc12c989
Frank Moreno <frankmoreno1993@gmail.com>
2019-08-14 10:58:49 -0500

    [U] Dart sdk 2.3 and RadarTile.vertices

--- radar_chart/example/lib/main.dart
+++ radar_chart/example/lib/main.dart
@@ -30,6 +30,23 @@ class _RadarChartExampleState extends State<RadarChartExample> {
   int _counter = 3;
   List<double> values1 = [0.4, 0.8, 0.65];
   List<double> values2 = [0.5, 0.3, 0.85];
+  List<PreferredSizeWidget> vertices2;
+  PreferredSizeWidget _vertex;
+
+  @override
+  void initState() {
+    super.initState();
+    const double radius = 5;
+    _vertex = PreferredSize(
+      preferredSize: Size(2 * radius, 2 * radius),
+      child: CircleAvatar(
+        backgroundColor: Colors.red,
+        radius: radius,
+      ),
+    );
+
+    vertices2 = [_vertex, _vertex, _vertex];
+  }
 
   void _incrementCounter() {
     setState(() {
@@ -37,6 +54,7 @@ class _RadarChartExampleState extends State<RadarChartExample> {
       _counter++;
       values1.add(random.nextDouble());
       values2.add(random.nextDouble());
+      vertices2.add(_vertex);
     });
   }
 
@@ -47,49 +65,51 @@ class _RadarChartExampleState extends State<RadarChartExample> {
         title: Text(widget.title),
       ),
       body: Container(
-          color: Colors.grey.shade200,
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 40,
-                  vertical: 10,
-                ),
-                child: Text(
-                  "There are ${values1.length} values for each radar chart!",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
+        color: Colors.grey.shade200,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 10,
+              ),
+              child: Text(
+                "There are ${values1.length} values for each radar chart!",
+                style: TextStyle(
+                  fontSize: 30,
                 ),
               ),
-              RadarChart(
-                length: _counter,
-                radius: 150,
-                initialAngle: 0,
-                //backgroundColor: Colors.white,
-                //borderStroke: 2,
-                //borderColor: Colors.red.withOpacity(0.4),
-                radialStroke: 2,
-                radialColor: Colors.grey.shade300,
-                radars: [
-                  RadarTile(
-                    values: values1,
-                    borderStroke: 2,
-                    borderColor: Colors.yellow,
-                    backgroundColor: Colors.yellow.withOpacity(0.4),
-                  ),
-                  RadarTile(
-                    values: values2,
-                    borderStroke: 2,
-                    borderColor: Colors.blue,
-                    backgroundColor: Colors.blue.withOpacity(0.4),
-                  ),
-                ],
-              ),
-            ],
-          )),
+            ),
+            RadarChart(
+              length: _counter,
+              radius: 150,
+              initialAngle: 0,
+              //backgroundColor: Colors.white,
+              //borderStroke: 2,
+              //borderColor: Colors.red.withOpacity(0.4),
+              radialStroke: 2,
+              radialColor: Colors.grey.shade300,
+              radars: [
+                RadarTile(
+                  values: values1,
+                  borderStroke: 2,
+                  borderColor: Colors.yellow,
+                  backgroundColor: Colors.yellow.withOpacity(0.4),
+                ),
+                RadarTile(
+                  values: values2,
+                  borderStroke: 2,
+                  borderColor: Colors.blue,
+                  backgroundColor: Colors.blue.withOpacity(0.4),
+                  vertices: vertices2,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
